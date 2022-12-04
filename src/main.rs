f9f5157faf7f1215875e4c1f3dea19e0c62da552
Sebastiano Faiella <xeno097.cp@gmail.com>
2022-12-04 13:41:30 -0400

    feat: implemented the Balances type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use std::{collections::HashMap, sync::RwLock};
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+struct Balances {
+    // RwLock is needed to safely share the same data between the different App instaces spawned by Actix.
+    balances: RwLock<HashMap<String, i128>>,
+}
+
+impl Balances {
+    pub fn new() -> Self {
+        Self {
+            balances: RwLock::new(HashMap::new()),
+        }
+    }
+}
 
 #[get("/balance/{address}")]
 async fn get_address_balance() -> impl Responder {
@@ -12,8 +27,11 @@ async fn send_transaction() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let balances = web::Data::new(Balances::new());
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(balances.clone())
             .service(get_address_balance)
             .service(send_transaction)
     })
